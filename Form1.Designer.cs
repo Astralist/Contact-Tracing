80e17fa000334d4b18050bca1423034468fdbf96
Astralist <83104822+Astralist@users.noreply.github.com>
2021-06-09 18:30:52 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -35,7 +36,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 43);
+            this.button1.Location = new System.Drawing.Point(30, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(225, 54);
             this.button1.TabIndex = 0;
@@ -53,6 +54,8 @@
             // 
             // button3
             // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.button3.Location = new System.Drawing.Point(30, 163);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(225, 54);
@@ -64,12 +67,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Contact Tracing";
             this.ResumeLayout(false);
 
         }
