192fd3ff699caeb08f810cb45fd0ea59804a810e
Astralist <83104822+Astralist@users.noreply.github.com>
2021-06-09 18:41:57 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -72,7 +72,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(297, 261);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
