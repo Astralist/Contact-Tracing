fe1d93bbb79c97eee49db8e6883c5a8b0aeeee28
Astralist <83104822+Astralist@users.noreply.github.com>
2021-06-09 18:33:35 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,8 @@
             // 
             // button1
             // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.button1.Location = new System.Drawing.Point(30, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(225, 54);
@@ -45,6 +47,8 @@
             // 
             // button2
             // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.button2.Location = new System.Drawing.Point(30, 103);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(225, 54);
